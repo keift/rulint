0074cd251a49912face2f38ec2aaa9cf1cc251c5
Fırat Özden <fir4tozden@gmail.com>
2026-05-25 20:33:56 +0300

    v1.1.9

--- tests/rules_table.test.ts
+++ tests/rules_table.test.ts
@@ -11,29 +11,43 @@ type Plugin = {
   rules: Record<string, { meta: { docs: { description: string } } }>;
 };
 
-const eslint_plugin_zod_rule_descriptions = Object.entries((ESLintPluginZod as unknown as Plugin).rules)
+const zod_rule_descriptions = Object.entries((ESLintPluginZod as unknown as Plugin).rules)
   .filter(([, value]) => value.meta.docs.description)
-  .map(([name, rule]) => ({ name, description: rule.meta.docs.description }));
+  .map(([name, rule]) => {
+    const description = rule.meta.docs.description.endsWith('.') ? rule.meta.docs.description : `${rule.meta.docs.description}.`;
+
+    return { name, description };
+  });
 
 const tseslint_rule_descriptions = Object.entries((TSESlint.plugin as unknown as Plugin).rules)
   .filter(([, value]) => value.meta.docs.description)
-  .map(([name, rule]) => ({ name, description: rule.meta.docs.description }));
+  .map(([name, rule]) => {
+    const description = rule.meta.docs.description.endsWith('.') ? rule.meta.docs.description : `${rule.meta.docs.description}.`;
 
-const eslint_rule_descriptions = ([...builtinRules.entries()] as [string, Plugin['rules'][string]][]).filter(([, value]) => value.meta.docs.description).map(([name, rule]) => ({ name, description: rule.meta.docs.description }));
+    return { name, description };
+  });
+
+const eslint_rule_descriptions = ([...builtinRules.entries()] as [string, Plugin['rules'][string]][])
+  .filter(([, value]) => value.meta.docs.description)
+  .map(([name, rule]) => {
+    const description = rule.meta.docs.description.endsWith('.') ? rule.meta.docs.description : `${rule.meta.docs.description}.`;
+
+    return { name, description };
+  });
 
 const zod_rules = Object.entries(ESLintPluginZod.configs.recommended.rules as Record<string, string | string[]>)
   .filter(([name, config]) => name.startsWith('zod') && (Array.isArray(config) ? config[0] === 'error' : config === 'error'))
-  .map(([name]) => [`[@${name}](https://github.com/marcalexiei/eslint-zod/blob/main/plugins/eslint-plugin-zod/docs/rules/${name.replaceAll('zod/', '')}.md)`, `${eslint_plugin_zod_rule_descriptions.find((rule) => rule.name === name.replaceAll('zod/', ''))?.description ?? 'None'}.`])
+  .map(([name]) => [`[@${name}](https://github.com/marcalexiei/eslint-zod/blob/main/plugins/eslint-plugin-zod/docs/rules/${name.replaceAll('zod/', '')}.md)`, zod_rule_descriptions.find((rule) => rule.name === name.replaceAll('zod/', ''))?.description ?? 'None.'])
   .sort((first, second) => (first[0] < second[0] ? -1 : first[0] > second[0] ? 1 : 0));
 
 const ts_rules = Object.entries(RulintOptionsDefault.ts?.rules as Record<string, string | string[]>)
   .filter(([name, config]) => name.startsWith('@typescript-eslint') && (Array.isArray(config) ? config[0] === 'error' : config === 'error'))
-  .map(([name]) => [`[${name}](https://typescript-eslint.io/rules/${name.replaceAll('@typescript-eslint/', '')})`, `${tseslint_rule_descriptions.find((rule) => rule.name === name.replaceAll('@typescript-eslint/', ''))?.description ?? 'None'}.`])
+  .map(([name]) => [`[${name}](https://typescript-eslint.io/rules/${name.replaceAll('@typescript-eslint/', '')})`, tseslint_rule_descriptions.find((rule) => rule.name === name.replaceAll('@typescript-eslint/', ''))?.description ?? 'None.'])
   .sort((first, second) => (first[0] < second[0] ? -1 : first[0] > second[0] ? 1 : 0));
 
 const js_rules = Object.entries(RulintOptionsDefault.js?.rules as Record<string, string | string[]>)
   .filter(([, config]) => (Array.isArray(config) ? config[0] === 'error' : config === 'error'))
-  .map(([name]) => [`[${name}](https://eslint.org/docs/latest/rules/${name})`, `${eslint_rule_descriptions.find((rule) => rule.name === name)?.description ?? 'None'}.`])
+  .map(([name]) => [`[${name}](https://eslint.org/docs/latest/rules/${name})`, eslint_rule_descriptions.find((rule) => rule.name === name)?.description ?? 'None.'])
   .sort((first, second) => (first[0] < second[0] ? -1 : first[0] > second[0] ? 1 : 0));
 
 const rules = [...zod_rules, ...ts_rules, ...js_rules];
