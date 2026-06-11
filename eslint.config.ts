279030542ff7058f19af6fbcc31749b6b0ef8e2f
Fırat Özden <fir4tozden@gmail.com>
2026-06-11 23:49:36 +0300

    v1.1.15

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,4 @@
 import { rulint } from 'rulint';
 import { naming } from '@rulint/naming';
 
-export default rulint({ configs: [naming({ naming: { variable: ['snake_case'] } })] });
+export default rulint({ configs: [naming({ naming: { function: ['snake_case'], variable: ['snake_case'] } })] });
